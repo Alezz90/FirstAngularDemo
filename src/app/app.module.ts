11d70fd7026513e97a7dd8e32de5143b0376ca02
Alezz90 <124068046+Alezz90@users.noreply.github.com>
2023-09-04 22:15:34 +0400

    FirstApp

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { StudentComponent } from './student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    StudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
+})
+export class StudentComponent {
+
+  showTable: boolean = false;
+
+ public student=[
+  {'name':'name1','Email':'name1@gmail','Marks':55},
+  {'name':'name2','Email':'name2@gmail','Marks':50},
+  {'name':'name3','Email':'name3@gmail','Marks':45}
+ ]
+ ShowTable(): void {
+  this.showTable = !this.showTable;
+}
+}
